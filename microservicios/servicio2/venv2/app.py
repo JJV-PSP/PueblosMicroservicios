12446761a258d05334f27d680365a688f7bf025d
Rebellant <158089230+Rebellant@users.noreply.github.com>
2025-02-13 21:16:35 +0100

    Entorno virtual 2
    - app.py creado
    - Falta solución de un error

---
+++ microservicios/servicio2/venv2/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import requests
+app2 = Flask(__name__)
+
+@app2.route('/<int:municipioid>/meteo', methods=['GET'])
+def get_meteo(municipioid):
+    from flask import Flask, jsonify
+import requests
+app2 = Flask(__name__)
+
+@app2.route('/<int:municipioid>/meteo', methods=['GET'])
+def get_meteo(municipioid):
+
+    try:
+        url = f"https://www.el-tiempo.net/api/json/v1/provincias/08/municipios/%7Bmunicipioid%7D"
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            return jsonify({"error": "No se pudo obtener el clima"}), 500
+
+        data = response.json()
+        return jsonify(data)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+if __name__ == '__main__':
+    app2.run(port=5001)
+    
