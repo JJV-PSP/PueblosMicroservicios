38fc7d78714893da4ad1e48311797998290a7719
DFran49 <francresmartin@gmail.com>
2025-02-13 20:24:43 +0100

    Servicio 1
    Completa la solicitud de datos con el id.

---
+++ microservicios/servicio1/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import json
+
+app1 = Flask(__name__)
+
+def cargar_datos():
+    with open('venv1/municipio.json', 'r', encoding='utf-8') as file:
+        return json.load(file)  
+
+@app1.route('/<int:municipioid>/geo', methods=['GET'])
+def get_geo(municipioid):
+    datos = cargar_datos()
+
+    if datos.get("municipioid") == municipioid:
+        return jsonify(datos)
+    else:
+        return jsonify({"error": "Municipio no encontrado"}), 404
+
+if __name__ == '__main__':
+    app1.run(port=5000)
+
