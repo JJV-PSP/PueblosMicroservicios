f69cc1f7557adb64c989c1e9d6715a7c9c31288d
DFran49 <francresmartin@gmail.com>
2025-02-20 20:33:59 +0100

    Arreglo servicio 2

--- microservicios/servicio2/venv2/app.py
+++ microservicios/servicio2/venv2/app.py
@@ -3,21 +3,19 @@ import requests
 app2 = Flask(__name__)
 
 
-@app2.route('/<int:municipioid>/meteo', methods=['GET'])
+@app2.route('/<string:municipioid>/meteo', methods=['GET'])
 def get_meteo(municipioid):
 
     try:
-        url = f"https://www.el-tiempo.net/api/json/v2/provincias/01/municipios/{municipioid}"
-        print(url)
-        response = jsonify(requests.get(url))
+        url = f"https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/{municipioid}"
+        response = requests.get(url)
        
-        print(response)
         #print(jsonify(data))
         if response.status_code != 200:
             return jsonify({"error": "No se pudo obtener el clima"}), 500
         
-        
-        return jsonify(response)
+        data = response.json()
+        return jsonify(data)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
