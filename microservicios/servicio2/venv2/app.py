762ee26f6182bf6183523548480b5913afc38700
Rebellant <158089230+Rebellant@users.noreply.github.com>
2025-02-20 19:58:36 +0100

    Entorno virtual 2 (Corregido)
    -Lee los datos
    - To do (Filtrar que datos queremos mostrar)

--- microservicios/servicio2/venv2/app.py
+++ microservicios/servicio2/venv2/app.py
@@ -2,17 +2,12 @@ from flask import Flask, jsonify
 import requests
 app2 = Flask(__name__)
 
-@app2.route('/<int:municipioid>/meteo', methods=['GET'])
-def get_meteo(municipioid):
-    from flask import Flask, jsonify
-import requests
-app2 = Flask(__name__)
 
 @app2.route('/<int:municipioid>/meteo', methods=['GET'])
 def get_meteo(municipioid):
 
     try:
-        url = f"https://www.el-tiempo.net/api/json/v1/provincias/08/municipios/%7Bmunicipioid%7D"
+        url = f"https://www.el-tiempo.net/api/json/v1/provincias/08/municipios/08022"
 
         response = requests.get(url)
         if response.status_code != 200:
